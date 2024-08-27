fdc0465d1e24537e873d2e0395f9c4cff0acc5ec
Abhirup Nandy <abhirupnandy.rocks@gmail.com>
2024-08-27 14:32:29 +0530

    Carousal component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js'
+	],
+    
 	theme: {
         // make default font roboto
         fontFamily: {
@@ -12,5 +16,7 @@ export default {
             },
         },
 	},
-	plugins: [],
+	plugins: [
+		require('flowbite/plugin')
+	],
 }
