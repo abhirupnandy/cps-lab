c6211cca7d69785139a571de41b44eb6617c24b2
DESKTOP-OVUK0OF\Admin <harshita.sharma@samarth.ac.in>
2024-09-07 18:01:49 +0530

    3rd commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+// import svgLoader from 'vite-svg-loader';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind()],
+  // vite: {
+  //   plugins: [svgLoader()]
+  // },
 });
